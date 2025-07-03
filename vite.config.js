1d2bcac7c2ec0cfee3e7ca83c7803805951e2734
timcoe429 <timcoe429@gmail.com>
2025-07-03 15:13:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  }
-})
+export default {
+  base: '/'
+}
