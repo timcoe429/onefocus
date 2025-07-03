c06e2cd5ac257e73ce92ab3f3372acf65f360d01
timcoe429 <timcoe429@gmail.com>
2025-07-03 14:20:24 -0400

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,25 @@
+import React from 'react'
+import { AppProvider } from './context/AppContext'
+import Header from './components/Layout/Header'
+import ProjectSelection from './components/ProjectSelection/ProjectGrid'
+import SprintMode from './components/Sprint/SprintMode'
+import { useSprint } from './hooks/useSprint'
 
+function AppContent() {
+  const { currentSprint } = useSprint()
+  
+  return (
+    <div className="app">
+      <Header />
+      {currentSprint ? <SprintMode /> : <ProjectSelection />}
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
+  )
+}
