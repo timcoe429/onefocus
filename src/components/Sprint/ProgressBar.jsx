93579f8bc18f05268f091d6d04cb657eb7ba91ab
timcoe429 <timcoe429@gmail.com>
2025-07-03 14:23:03 -0400

    Update ProgressBar.jsx

--- src/components/Sprint/ProgressBar.jsx
+++ src/components/Sprint/ProgressBar.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import styles from './Sprint.module.css'
 
+export default function ProgressBar({ percentage }) {
+  return (
+    <div className={styles.progressContainer}>
+      <div className={styles.progressBar}>
+        <div 
+          className={styles.progressFill} 
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+      <span className={styles.progressText}>{percentage}%</span>
+    </div>
+  )
+}
