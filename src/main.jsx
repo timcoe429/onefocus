74d72d7fc2747220e369e6bc49310d3f20a31e41
timcoe429 <timcoe429@gmail.com>
2025-07-03 17:13:41 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'  // Add the .jsx extension
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App.jsx'
+import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
