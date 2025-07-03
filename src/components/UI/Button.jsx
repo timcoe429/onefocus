981c851855ad09719c33c4c6691a486238aa36ba
timcoe429 <timcoe429@gmail.com>
2025-07-03 14:23:27 -0400

    Update Button.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import styles from './UI.module.css'
 
+export default function Button({ 
+  children, 
+  variant = 'default', 
+  onClick, 
+  className = '',
+  ...props 
+}) {
+  return (
+    <button
+      className={`${styles.button} ${styles[variant]} ${className}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
