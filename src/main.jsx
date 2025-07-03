32ac85a65af3125c2e0a06ca43fef4b52f664044
timcoe429 <timcoe429@gmail.com>
2025-07-03 14:32:44 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'  // Add the .jsx extension
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
